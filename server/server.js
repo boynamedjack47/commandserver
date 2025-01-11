1d6cf46ddad5ac6aa064bbc8c7b8367ee640b058
Jack Barker <boynamedjack@Jacks-MacBook-Pro.local>
2025-01-11 16:01:31 -0700

    Added /w feature to allow each client to whisper to each other. Will add the kick and username feature next

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const server = new WebSocket.Server({ port: PORT });
 const clients = new Map(); // Store clients and their IDs
 const logFilePath = path.join(__dirname, 'chat.log');
 
-// Append messages to chaat.log
+// Append messages to chat.log
 const logMessage = (message) => {
     fs.appendFileSync(logFilePath, message + '\n');
 };
@@ -53,5 +53,53 @@ server.on('connection', (ws) => {
         }
     });
 });
+
+// Command Functions
+const handleCommand = (ws, message, sender) => {
+    const [command, ...args] = message.split(' ');
+
+    switch (command) {
+        case '/w':
+            whisper(ws, args, sender);
+            break;
+        case '/username':
+            changeUsername(ws, args, sender);
+            break;
+        case '/kick':
+            kickClient(ws, args, sender);
+            break;
+        case '/clientlist':
+            listClients(ws);
+            break;
+        default:
+            ws.send(`Unknown command: ${command}`);
+    }
+};
+
+// /w - Whisper to another client
+const whisper = (ws, args, sender) => {
+    if (args.length < 2) {
+        ws.send('Error: /w command requires a username and a message.');
+        return;
+    }
+
+    const recipientName = args[0];
+    const message = args.slice(1).join(' ');
+
+    const recipient = [...clients].find(([client, data]) => data.username === recipientName);
+
+    if (!recipient) {
+        ws.send(`Error: User ${recipientName} not found.`);
+        return;
+    }
+
+    if (recipient[0] === ws) {
+        ws.send('Error: You cannot whisper to yourself.');
+        return;
+    }
+
+    recipient[0].send(`Whisper from ${sender}: ${message}`);
+    logMessage(`[WHISPER] ${sender} whispered to ${recipientName}: ${message}`);
+};
 
 console.log(`Chat server running on ws://localhost:${PORT}`);
